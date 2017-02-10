8738fa04168b1a7c19c96783fadeebcce6fbd920
Odinei Rocha <odinei.ribeiro@live.com>
2017-02-10 17:36:07 -0200

    new colors to easter egg

--- js/est_egg.js
+++ js/est_egg.js
@@ -1,6 +1,6 @@
 function changeStyle() {
   x = document.getElementById("hello");
-  colorsArray = ['#499491', '#b31313', '#7984ee', '#9ed763', '#f54ea2',
-                 '#a64942', '#272932', '#f7f0e9', '#3a4750', '#2c5d63']
+  colorsArray = ['#000000', '#EC576B', '#4ECSC1', '#ESE358', '#015249',
+                 '#6B7A8F', '#F7882F', '#6E3667', '#FEDC3D', '#999999']
   x.style.color = colorsArray[Math.floor(Math.random() * colorsArray.length)];
 }
