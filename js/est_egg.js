bc76f582624794cee3de71d6a76dd4c94067029f
Odinei Rocha <odinei.ribeiro@live.com>
2017-06-15 22:39:20 -0300

    new front end!

--- js/est_egg.js
+++
@@ -1,6 +0,0 @@
-function changeStyle() {
-  x = document.getElementById("hello");
-  colorsArray = ['#000000', '#EC576B', '#4ECSC1', '#ESE358', '#015249',
-                 '#6B7A8F', '#F7882F', '#6E3667', '#FEDC3D', '#999999']
-  x.style.color = colorsArray[Math.floor(Math.random() * colorsArray.length)];
-}
